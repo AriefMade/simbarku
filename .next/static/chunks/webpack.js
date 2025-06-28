109abaa138cc22525b7c4870c17cda691ade5257
AriefMade <ariepmade@gmail.com>
2025-06-28 13:32:02 +0800

    commit lagi, sebenernya .next aja sih

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "54a5f6fa4d3711d4"; }
+/******/ 		__webpack_require__.h = function() { return "4f6e3178700432af"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
